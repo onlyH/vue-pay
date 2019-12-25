384757215b9e736c66ae0e323fe1c0b410c0fbfe
shaun-sheep <pdsjjxiaoh@126.com>
2019-12-25 11:25:31 +0800

    '修改host'

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   devServer: {
     open: process.platform === 'darwin',
-    host: '0.0.0.0',//改为域名
-    port: 23333,//改为端口80
+    host: '0.0.0.0',
+    port: 23333,
     https: false,
     hotOnly: false,
     proxy: null, // string | Object
